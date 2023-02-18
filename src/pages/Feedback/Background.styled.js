1c374d9d8cbc69cae3535dac4477b411312ee379
MarynaPyvovar <marina18renkas@gmail.com>
2023-02-18 12:12:30 +0200

    add backgrounds

--- src/pages/Feedback/Background.styled.js
+++ src/pages/Feedback/Background.styled.js
@@ -3,6 +3,8 @@ import map from '../../assets/backgrounds/map-image.png';
 import pink from '../../assets/backgrounds/pink.svg';
 import yellowUp from '../../assets/backgrounds/yellow-up.svg';
 import yellowDown from '../../assets/backgrounds/yellow-down.svg';
+import cloud from '../../assets/backgrounds/cloud.png';
+import yelltr from '../../assets/backgrounds/yell-tr.png';
 
 export const Wrapper = styled.div`
   position: relative;
@@ -75,5 +77,112 @@ export const YellowDownDecorBall = styled.div`
   @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
     top: 515px;
     left: 750px;
+  }
+`;
+
+export const YellowTransparentBall = styled.div`
+  position: absolute;
+  top: 21px;
+  left: 350px;
+  width: 87px;
+  height: 87px;
+  background-image: url(${yelltr});
+  background-size: cover;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    top: 21px;
+    left: 650px;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    top: 21px;
+    left: 750px;
+  }
+`;
+
+export const Cloud1 = styled.div`
+  position: absolute;
+  top: 770px;
+  left: 233px;
+  width: 288px;
+  height: 200px;
+  background-image: url(${cloud});
+  background-size: cover;
+  z-index: -1;
+`;
+
+export const Cloud2 = styled.div`
+  position: absolute;
+  top: 699px;
+  left: 193px;
+  width: 131px;
+  height: 100px;
+  background-image: url(${cloud});
+  background-size: cover;
+  z-index: -1;
+`;
+
+export const Cloud3 = styled.div`
+  position: absolute;
+  top: 745px;
+  left: 823px;
+  width: 170px;
+  height: 128px;
+  background-image: url(${cloud});
+  background-size: cover;
+  z-index: -1;
+`;
+
+export const Cloud4 = styled.div`
+  position: absolute;
+  top: 625px;
+  left: 435px;
+  width: 218px;
+  height: 164px;
+  background-image: url(${cloud});
+  background-size: cover;
+  z-index: -1;
+`;
+
+export const Cloud5 = styled.div`
+  position: absolute;
+  top: 400px;
+  left: 0;
+  width: 218px;
+  height: 164px;
+  background-image: url(${cloud});
+  background-size: cover;
+  z-index: -1;
+`;
+
+export const Cloud6 = styled.div`
+  position: absolute;
+  top: 185px;
+  left: 23px;
+  width: 245px;
+  height: 184px;
+  background-image: url(${cloud});
+  background-size: cover;
+  z-index: -1;
+`;
+
+export const Cloud7 = styled.div`
+  position: absolute;
+  top: 130px;
+  left: 400px;
+  width: 138px;
+  height: 103px;
+  background-image: url(${cloud});
+  background-size: cover;
+  z-index: -1;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    top: 130px;
+    left: 600px;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    top: 130px;
+    left: 708px;
   }
 `;
